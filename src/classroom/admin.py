c17e803442b133958244c51c4c68cf8e474eed61
vinayak <itssvinayak@gmail.com>
2020-12-11 15:07:30 +0530

    test

--- src/classroom/admin.py
+++ src/classroom/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Announcement)
-admin.site.register(Subject)
-admin.site.register(ClassRoom)
+# admin.site.register(Subject)
+
+
+
+class SubjectInline(admin.TabularInline):
+    model = ClassRoom.subject.through
+    extra = 0
+
+@admin.register(ClassRoom)
+class ClassRoomAdmin(admin.ModelAdmin):
+    inlines=[SubjectInline]
+    exclude=('subject',)
+
+
+
 admin.site.register(Assignment)
